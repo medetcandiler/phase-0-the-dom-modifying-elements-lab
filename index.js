120d5d98be5e443bd3ca1615009a00eea4673967
Medetcan Diler <medetdiler1996@gmail.com>
2023-04-21 19:05:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const main = document.querySelector('main#main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = 'Medetcan Diler is the champion';
